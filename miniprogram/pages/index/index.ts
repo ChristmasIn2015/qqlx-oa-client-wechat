cb3e37d06e81dbd4a1ef14498fcb461dde836726
huangwenqiang <wqao1023@qq.com>
2023-03-18 23:51:33 +0800

    'update'

---
+++ miniprogram/pages/index/index.ts
@@ -0,0 +1,18 @@
+// index.ts
+// 获取应用实例
+const app = getApp<IAppOption>()
+
+Page({
+  data: {
+    index: 'https://qqlx.tech',
+  },
+  onLoad() {
+    // @ts-ignore
+    wx.login({
+      success: res => {
+        this.setData({index: 'https://qqlx.tech/oa-client/login' + '?code=' + res.code})
+      },
+      fail: (e)=>console.log(e)
+    })
+  },
+})
